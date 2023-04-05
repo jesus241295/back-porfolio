3f918ddb71a0ce12bec0fc439d608dfd8cc229ee
jesus241295 <jesusgonvale@gmail.com>
2023-04-05 17:26:54 -0500

    The model was changed to home, the home was added to the database

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+mongoose.pluralize(null);
 const app = express();
+const homeRouter = require("./routers/home.router");
 
 const BD_USER = process.env.BD_USER;
 const BD_PASS = process.env.BD_PASS;
 const BD_HOST = process.env.BD_HOST;
 const BD_NAME = process.env.BD_NAME;
 const PORT = process.env.PORT;
+app.use(express.json());
 
 const URL = `mongodb+srv://${BD_USER}:${BD_PASS}@${BD_HOST}/${BD_NAME}?retryWrites=true&w=majority`;
+app.use("/home", homeRouter);
 
 mongoose
   .connect(URL)
